c2205d2c4051796314192a46840a4c5ead7c2e30
Charles Ye <charles@cmefi.com>
2021-03-20 15:27:03 -0400

    Improved page styling, added time series plot and subfrequency aggregation

--- app-homepage/app.r
+++ app-homepage/app.r
@@ -37,51 +37,82 @@ ui = tagList(
 	
 	
 	div(
-		class = 'container-fluid',
+		class = 'container-fluid p-0',
 		
 		div(
-			class = 'container',
-			h3('AW GUI')
-			),
-		div(
-			class = 'container',
+			class = 'container-fluid pt-3 pb-4',
 			div(
+				class = 'container',
 				div(
-					class = 'row',
+					class = 'row justify-content-center',
 					div(
-						class = 'btn-group',
-						role = 'group',
-						actionButton('prev', 'Back', class = 'btn-primary'),
-						actionButton('currentdate', 'Current Date', class = 'btn-primary disabled'),
-						actionButton('fwd', 'Next', class = 'btn-primary')
+						class = 'col-auto',
+						h3('ActivityWatch Interface'),
 					),
-					selectInput(
-						'freq',
-						h4('Select Me'),
-						choices = list('Hour' = 'h', 'Day' = 'd', "Week" = 'w', 'Month' = 'm'), selected = 'd'
+					div(
+						class = 'col-auto',
+						selectInput(
+							'freq',
+							label = NULL,
+							choices = list('Hour' = 'h', 'Day' = 'd', "Week" = 'w', 'Month' = 'm'), selected = 'w'
 						)
 					)
+
+				),
+				div(
+					class = 'row justify-content-center my-2',
+					div(
+						class = 'col-auto',
+						div(
+							class = 'btn-group',
+							role = 'group',
+							actionButton('prev', 'Back', class = 'btn-primary'),
+							actionButton('currentdate', 'Current Date', class = 'btn-primary disabled'),
+							actionButton('fwd', 'Next', class = 'btn-primary')
+						)
+						
+					)
+				),
+				div(
+					class = 'row justify-content-center',
+					div(class = 'col-sm-12 col-md-12 col-lg-12 col-xl-12', highchartOutput('tsPlot', height = 150)),
 				)
-			),
+				
+			)
+		),
+		# div(
+		# 	class = 'container',
+		# 	div(
+		# 		class = 'row justify-content-center my-4',
+		# 		div(class = 'col-sm-12 col-md-12 col-lg-12 col-xl-12', highchartOutput('tsPlot', height = 150)),
+		# 	)
+		# ),
 		
 		div(
-			class = 'container',
+			class = 'container-fluid',
+			style = 'height:2rem;background-image:linear-gradient(45deg, var(--bs-econgreen) 50%, var(--bs-econpale) 50%)',
 			div(
-				class = 'row',
-				div(class = 'col-md-12 col-lg-6', highchartOutput('catPlot', height = 300)),
-				div(class = 'col-md-12 col-lg-6', dataTableOutput('catTable')),
-				)
-			),
-		
+				class = 'container',
+				h4('Data', style = 'color:white')
+			)
+		),
 		div(
-			class = 'container',
+			class = 'container-fluid py-4',
+			style = 'background-image: linear-gradient(165deg, rgba(235, 245, 247, .5) 50%, rgba(255, 255, 255, .3) 50%)',
 			div(
-				class = 'row justify-content-center',
-				highchartOutput(outputId = "taskPlot"),
-				div(class = 'col-auto', dataTableOutput('taskTable'))
+				class= 'container',
+				div(
+					class = 'row',
+					div(class = 'col-md-12 col-lg-6', highchartOutput('catPlot', height = 300)),
+					div(class = 'col-md-12 col-lg-6', style = 'font-size: .75rem', dataTableOutput('catTable')),
+				),
+				div(
+					class = 'row justify-content-center',
+					highchartOutput(outputId = "taskPlot"),
+					div(class = 'col-auto', dataTableOutput('taskTable'))
 				)
-			),
-		
+			)
+		),
 		hr(),
 		div(
 			id = 'settings-container',
@@ -123,33 +154,79 @@ server = function(input, output, session) {
 		{c(., .$x)}
 	
 	
-	getRawDf = reactive({
-		conn = dbConnect(RSQLite::SQLite(), SQLITE_DIR)
-		rawDf =
-			dbGetQuery(conn, 'SELECT * FROM eventmodel WHERE duration >= 1 ORDER BY timestamp ASC') %>%
-			as_tibble(.) %>%
-			dplyr::mutate(., afk = ifelse(bucket_id == 1, ifelse(datastr == '{\"status\": \"afk\"}', TRUE, FALSE), NA)) %>%
-			tidyr::fill(., afk) 
-		return(rawDf)
-	})
-	
-	
+	# Debug
+	# input = list(freq = 'w')
+	# state = list()
 	state = reactiveValues(
 		freq = NULL,
-		dateMax = NULL
+		dateMax = NULL,
+		subFreq = NULL,
+		subFreqDf = NULL
 		)
 	
 	
 	
-	
 	observeEvent(input$freq, {
+		
 		state$freq = input$freq
-		state$dateMax = Sys.time()
+
+		state$dateMax = {
+			if (state$freq == 'h') lubridate::as_datetime(Sys.time(), tz = Sys.timezone())
+			else if (state$freq == 'd') lubridate::ceiling_date(Sys.time() - lubridate::hours(4), 'day') + lubridate::hours(4)
+			else if (state$freq == 'w') lubridate::as_datetime(lubridate::ceiling_date(Sys.time(), 'week', week_start = 1), tz = Sys.timezone()) + lubridate::hours(4)
+			else if (state$freq == 'm') lubridate::as_datetime(lubridate::ceiling_date(Sys.time(), 'month'), tz = Sys.timezone()) + lubridate::hours(4)
+		}
 		state$dateMin = {
 			if (state$freq == 'h') lubridate::as_datetime(Sys.time() - lubridate::hours(1), tz = Sys.timezone())
 			else if (state$freq == 'd') lubridate::floor_date(Sys.time() - lubridate::hours(4), 'day') + lubridate::hours(4)
 			else if (state$freq == 'w') lubridate::as_datetime(lubridate::floor_date(Sys.time(), 'week', week_start = 1), tz = Sys.timezone()) + lubridate::hours(4)
-			else if (state$freq == 'm') lubridate::as_datetime(lubridate::floor_date(Sys.time(), 'month'), tz = Sys.timezone())
+			else if (state$freq == 'm') lubridate::as_datetime(lubridate::floor_date(Sys.time(), 'month'), tz = Sys.timezone()) + lubridate::hours(4)
+		}
+		
+		state$subFreq = {
+			if (state$freq == 'm') 'md'
+			else if (state$freq == 'w') 'd'
+			else if (state$freq == 'd') 'h'
+			else if (state$freq == 'h') 'm'
+		}
+		
+		state$subFreqDf = {
+			if (state$subFreq == 'md')
+				tibble(
+					order = 1:length(seq(state$dateMin, state$dateMax, by = '1 day')),
+					time_label = seq(state$dateMin, state$dateMax, by = '1 day') %>% format(., '%m/%d'),
+					start = seq(state$dateMin, state$dateMax, by = '1 day'),
+					end = seq(state$dateMin, state$dateMax, by = '1 day') + days(1) - seconds(1)
+				)
+			
+			else if (state$subFreq == 'd')
+				tibble(
+					order = 1:7,
+					time_label = c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'),
+					start = seq(state$dateMin, state$dateMax - lubridate::days(1), by = '1 day'),
+					end = seq(state$dateMin, state$dateMax - lubridate::days(1), by = '1 day') + days(1) - seconds(1)
+				)
+			
+			else if (state$subFreq == 'h')
+				tibble(
+					order = 1:24,
+					time_label = seq(state$dateMin, state$dateMax, by = '1 hour') %>% head(., -1) %>% {paste0(as.numeric(format(., '%I')), str_to_lower(str_sub(format(., '%p'), 1, 1)))},
+					start = seq(state$dateMin, state$dateMax, by = '1 hour') %>% head(., -1),
+					end = (seq(state$dateMin, state$dateMax, by = '1 hour') + hours(1) - seconds(1))%>% head(., -1)
+				)
+			else if (state$subFreq == 'm')
+				tibble(
+					order = 1:60,
+					time_label = seq(state$dateMin, state$dateMax, by = '1 min') %>% format(., '%I:%M'),
+					start = seq(state$dateMin, state$dateMax, by = '1 min'),
+					end = seq(state$dateMin, state$dateMax, by = '1 min') + minutes(1) - seconds(1)
+				)
+			
+			else NULL
+			# else if (subFreq == 'h')
+			# 	order = 4:24,
+			# 	time_label = as.character(4:24),
+			# 	start = 
 		}
 		
 		updateActionButton(
@@ -166,7 +243,6 @@ server = function(input, output, session) {
 					' (', toupper(state$freq), ')'
 				)
 			)
-		
 		
 	}, ignoreInit = FALSE)
 	
@@ -205,6 +281,7 @@ server = function(input, output, session) {
 	}, ignoreInit = TRUE)
 	
 	
+	# Update displayed date 
 	observeEvent(c(input$fwd, input$prev, input$freq), {
 		updateActionButton(
 			session,
@@ -214,7 +291,7 @@ server = function(input, output, session) {
 					{
 						if (state$freq == 'h') paste0(format(state$dateMin, '%I:%M%p'), ' - ', format(state$dateMax, '%I:%M%p'))
 						else if (state$freq == 'd') format(state$dateMin, '%A %B %d')
-						else if (state$freq == 'w') paste0(format(state$dateMin, '%A %b %d'), ' - ', format(state$dateMax - lubridate::days(1), '%A %b %d'))
+						else if (state$freq == 'w') paste0(format(state$dateMin, '%A %b %d'), ' - ', format(state$dateMax - lubridate::days(0), '%A %b %d'))
 						else if (state$freq == 'm') paste0(format(state$dateMin, '%m/%d/%Y'))
 					},
 					' (', toupper(state$freq), ')'
@@ -222,14 +299,23 @@ server = function(input, output, session) {
 		)
 	}, ignoreInit = FALSE)
 	
+	
+	# Get SQL data
+	getRawDf = reactive({
+		conn = dbConnect(RSQLite::SQLite(), SQLITE_DIR)
+		rawDf =
+			dbGetQuery(conn, 'SELECT * FROM eventmodel WHERE duration >= 1 ORDER BY timestamp ASC') %>%
+			as_tibble(.) %>%
+			dplyr::mutate(., afk = ifelse(bucket_id == 1, ifelse(datastr == '{\"status\": \"afk\"}', TRUE, FALSE), NA)) %>%
+			tidyr::fill(., afk) 
+		return(rawDf)
+	})
+	
+	# Filter data by date range
 	getFilteredDf = reactive({
 		
 		rawDf = getRawDf()
-		
-		# dateMax = if (dateMax == )
-		
 
-		
 		filteredDf =
 			rawDf %>%
 			# Adjust to system timezone
@@ -240,11 +326,12 @@ server = function(input, output, session) {
 	})
 	
 	
+	# Return list of fully aggregated df (taskTimeDf) and daily grouped df (subTaskTimeDf)
 	getTaskTimeDf = reactive({
 		
 		filteredDf = getFilteredDf()
 		
-		taskTimeDf =
+		taskTimeDf0 =
 			filteredDf %>%
 			dplyr::filter(., (bucket_id == 1 & afk == TRUE) | bucket_id != 1 & duration != 0) %>%
 			rowwise(.) %>%
@@ -276,38 +363,151 @@ server = function(input, output, session) {
 			dplyr::filter(., afk == FALSE) %>%
 			# Order desc
 			dplyr::arrange(., desc(timestamp)) %>%
-			dplyr::mutate(., task = ifelse(is.na(url), app, url)) %>%
-			dplyr::group_by(., task) %>%
-			dplyr::summarize(., minutes = round(sum(duration)/60)) %>%
+			dplyr::mutate(., task = ifelse(is.na(url), app, url))
+		
+		
+		# Aggregate up to subfrequency and clean
+		subTaskTimeDf =
+			state$subFreqDf %>%
+			dplyr::group_by(., order) %>%
+			dplyr::group_split(.) %>%
+			lapply(., function(subFreqDf)
+				dplyr::filter(taskTimeDf0, timestamp >= subFreqDf$start & timestamp <= subFreqDf$end) %>%
+					dplyr::bind_cols(., subFreqDf[, c('order', 'time_label')])
+				) %>%
+			dplyr::bind_rows(.) %>%
+			dplyr::group_by(
+				.,
+				task,
+				order,
+				time_label,
+				) %>%
+			dplyr::summarize(., minutes = round(sum(duration)/60), .groups = 'drop') %>%
 			dplyr::mutate(., hours = round(minutes/60, 2)) %>%
 			dplyr::left_join(., taskDf %>% dplyr::mutate(., task = str_replace(task, coll('*.'), '')), by = 'task') %>%
 			dplyr::mutate(., category = ifelse(is.na(category), 'unknown', category)) %>%
 			dplyr::arrange(., desc(minutes)) %>%
 			dplyr::filter(., task != 'LockApp.exe')
 		
-		return(taskTimeDf)
+		taskTimeDf =
+			subTaskTimeDf %>%
+			dplyr::group_by(., task) %>%
+			dplyr::summarize(., minutes = sum(minutes), hours = sum(hours), category = head(category, 1)) %>%
+			dplyr::arrange(., desc(minutes))
+			
+		return(list(taskTimeDf = taskTimeDf, subTaskTimeDf = subTaskTimeDf))
 	})
 	
 	
 	getCatTimeDf = reactive({
 		
-		taskTimeDf = getTaskTimeDf()
-		
-		catTimeDf =
-			taskTimeDf %>%
-			dplyr::group_by(., category) %>%
-			dplyr::summarize(., minutes = sum(minutes)) %>% 
+		subTaskTimeDf = getTaskTimeDf()$subTaskTimeDf
+
+		subCatTimeDf =
+			subTaskTimeDf %>%
+			dplyr::group_by(., category, time_label) %>%
+			dplyr::summarize(., minutes = sum(minutes), .groups = 'drop') %>% 
 			dplyr::mutate(., hours = round(minutes/60, 2)) %>%
 			dplyr::left_join(., catDf, by = 'category')
-			
-		return(catTimeDf)
+		
+		
+		catTimeDf =
+			subCatTimeDf %>%
+			dplyr::group_by(., category, name, prod_index, parent_category, color, rgbaColor) %>%
+			dplyr::summarize(., minutes = sum(minutes), hours = sum(hours), .groups = 'drop')
+
+		return(list(subCatTimeDf = subCatTimeDf, catTimeDf = catTimeDf))
 	})
 	
 	
+	# getProdTimeDf = reactive({
+	# 	
+	# 	catTimeDf = getCatTimeDf()$catTimeDf
+	# 	
+	# 	prodTimeDf =
+	# 		catTimeDf %>%
+	# 		dplyr::group_by(., prod_index) %>%
+	# 		dplyr::summarize(., minutes = sum(minutes)) %>%
+	# 		dplyr::mutate(., hours = round(minutes/60, 2))
+	# 	
+	# 	return(prodTimeDf)
+	# 	
+	# })
+	
+	
+	
+	# output$prodPlot =
+	# 	renderHighchart({
+	# 		chartDf =
+	# 			getProdTimeDf() %>%
+	# 			dplyr::mutate(., time = {if (max(.$minutes) >= 120) hours else minutes})
+	# 		
+	# 		textStr = {if (max(chartDf$minutes) >= 120) 'Duration (Hours)' else 'Duration (Minutes)'}
+	# 		
+	# 		highchart() %>%
+	# 			hc_add_series(
+	# 				.,
+	# 				type = 'column',
+	# 				data = chartDf,
+	# 				mapping = hcaes(x = prod_index, y = time, color = prod_index),
+	# 				dataLabels = list(enabled = TRUE)
+	# 				) %>%
+	# 			hc_xAxis(categories = chartDf$prod_index) %>%
+	# 			hc_yAxis(title = list(text = textStr)) %>%
+	# 			hc_size(height = 200) %>%
+	# 			hc_legend(enabled = FALSE) %>%
+	# 			hc_add_theme(hc_theme_ft())
+	# 	})
+	
+	output$tsPlot = 
+		renderHighchart({
+		
+			# https://stackoverflow.com/questions/46671973/highcharter-stacked-column-groupings-not-using-hchart
+			subCatTimeDf = getCatTimeDf()$subCatTimeDf
+			
+			seriesList =
+				purrr::cross_df(list(category = catDf$category, time_label = state$subFreqDf$time_label)) %>%
+				dplyr::left_join(
+					.,
+					subCatTimeDf[, c('category', 'time_label', 'minutes', 'hours')],
+					by = c('time_label', 'category')
+				) %>%
+				dplyr::left_join(., catDf, by = 'category') %>%
+				dplyr::mutate(
+					.,
+					minutes = ifelse(is.na(minutes), 0, minutes),
+					hours = ifelse(is.na(hours), 0, hours)
+				) %>%
+				dplyr::group_by(., category, name, color) %>%
+				dplyr::group_split(.) %>%
+				lapply(., function(df)
+					list(
+						name = head(df$name, 1),
+						type = 'column',
+						color = head(df$color, 1),
+						data = purrr::transpose(df) %>% purrr::map(., ~ list(.$time_label, {if (max(subCatTimeDf$minutes) >= 120) .$hours else .$minutes}))
+					)
+				)
+			
+			textStr = {if (max(subCatTimeDf$minutes) >= 120) 'Duration (Hours)' else 'Duration (Minutes)'}
+			
+			highchart() %>%
+				hc_chart(borderColor = 'black', borderWidth = 1, borderRadius = 5) %>%
+				hc_add_series_list(seriesList) %>%
+				hc_xAxis(., categories = state$subFreqDf$time_label) %>%
+				hc_plotOptions(column = list(stacking = "normal")) %>%
+				hc_legend(enabled = FALSE) %>%
+				hc_add_theme(hc_theme_538())
+
+		})
+	
 	output$catPlot =
 		renderHighchart({
+			
+			catTimeDf = getCatTimeDf()$catTimeDf
+			
 			chartDf =
-				getCatTimeDf() %>%
+				catTimeDf %>%
 				dplyr::mutate(., time = {if (max(.$minutes) >= 120) hours else minutes})
 			
 			textStr = {if (max(chartDf$minutes) >= 120) 'Duration (Hours)' else 'Duration (Minutes)'}
@@ -324,8 +524,11 @@ server = function(input, output, session) {
 	
 	output$catTable =
 		renderDT({
+			
+			catTimeDf = getCatTimeDf()$catTimeDf
+			
 			datatable(
-				getCatTimeDf() %>%
+				catTimeDf %>%
 					rowwise(.) %>%
 					dplyr::mutate(
 						.,
@@ -347,8 +550,10 @@ server = function(input, output, session) {
 	output$taskPlot =
 		renderHighchart2({
 			
+			taskTimeDf = getTaskTimeDf()$taskTimeDf
+			
 			chartDf =
-				getTaskTimeDf() %>%
+				taskTimeDf %>%
 				head(., 20) %>%
 				dplyr::left_join(., catDf, by = 'category') %>%
 				dplyr::mutate(., time = {if (max(.$minutes) >= 120) hours else minutes})
@@ -366,8 +571,11 @@ server = function(input, output, session) {
 
 	output$taskTable =
 		DT::renderDT({
+			
+			taskTimeDf = getTaskTimeDf()$taskTimeDf
+			
 			datatable(
-				getTaskTimeDf() %>%
+				taskTimeDf %>%
 					dplyr::select(., task, category, minutes),
 				colnames = c('Task' = 'task', 'Category' = 'category', 'Duration (Minutes)' = 'minutes'),
 				editable = 'row',
@@ -404,6 +612,8 @@ server = function(input, output, session) {
 						)
 					)
 			})
+	
+
 	
 	output$taskParams = 
 		DT::renderDT({
